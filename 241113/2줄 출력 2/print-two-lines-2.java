27dbcb4e3c7f549680ed47de7dfc28184a7e737a
HyeonSeong <a01062704312@gmail.com>
2024-11-13 09:03:55 +0900

    Title: 2줄 출력 2, Time: 87ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 241113/2줄 출력 2/print-two-lines-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+        sb.append("Do you know Kimchi?");
+        sb.append(System.lineSeparator());
+        sb.append("My Favorite Korean Food Is Kimchi!");
+        System.out.println(sb);
+    }
+}
