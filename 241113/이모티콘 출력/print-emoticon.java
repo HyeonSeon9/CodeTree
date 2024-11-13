41d73319a0a242fc5a3e2eb8b5bcbc869d67018f
HyeonSeong <a01062704312@gmail.com>
2024-11-13 09:19:34 +0900

    Title: 이모티콘 출력, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 241113/이모티콘 출력/print-emoticon.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        StringBuilder sb = new StringBuilder();
+        sb.append("(*´∪`)");
+        sb.append(System.lineSeparator());
+        sb.append("(^・ω・^)");
+        sb.append(System.lineSeparator());
+        sb.append("(=^･^=)");
+        System.out.println(sb);
+    }
+}
